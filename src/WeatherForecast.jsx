721d22a88811750cc6516aff2ab77d4626124cbf
KeviinDavis <daviskevin1@hotmail.com>
2024-11-18 08:28:50 -0500

    completed

---
+++ src/WeatherForecast.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './WeatherForecast.css';
+
+function WeatherForecast(props) {
+  return (
+    <div className="weather">
+      <h2>{props.day}</h2>
+      <img src={props.img} alt={props.imgAlt} />
+      <p>
+        <span>Conditions: </span>{props.conditions}
+      </p>
+      <p>
+        <span>Time: </span>{props.time}
+      </p>
+    </div>
+  );
+}
+
+export default WeatherForecast;
